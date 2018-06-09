693f381a3546a3c378409fc05b9659a5a776a8a5
Luka Rolak <lukarolak2@gmail.com>
2018-06-09 18:49:11 +0200

    Create A+B.py

---
+++ A-B/A+B.py
@@ -0,0 +1,10 @@
+'''
+A+B   ─── a classic problem in programming contests,   it's given so contestants can gain familiarity with the online judging system being used.
+Task:
+Given two integer,   A and B.
+Their sum needs to be calculated.
+Input data
+Two integers are written in the input stream, separated by space(s):
+'''
+a,b = int(input()),int(input())
+print(a+b)
